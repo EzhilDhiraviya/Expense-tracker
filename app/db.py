7c9d109a2830411e82ca77e624fa166ae1640982
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-06-30 14:46:37 +0530

    add route - completed

--- app/db.py
+++ app/db.py
@@ -2,13 +2,14 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+# uri = "mongodb+srv://ezhildhiraviya:TbdZoOXC4WovYQgO@exp-cluster.fmzwmi5.mongodb.net/?appName=exp-cluster"
+uri = os.getenv('MONGODB_URI') #final
 db=None
-# uri = os.getenv('MONGODB_URI')
-# Create a new client and connect to the server
-uri = "mongodb+srv://ezhildhiraviya:TbdZoOXC4WovYQgO@exp-cluster.fmzwmi5.mongodb.net/?appName=exp-cluster"
-client = MongoClient(uri, server_api=ServerApi('1'))
 
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
 db=client['expenses-db']
+
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
