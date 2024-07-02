14b898f5ba81306a861485af99d050c2934df20f
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-07-02 18:30:15 +0530

    final

--- app/routes/fetchMonth.py
+++ app/routes/fetchMonth.py
@@ -2,21 +2,25 @@ from .. import db as d
 from .. import app
 from flask import request, jsonify, render_template
 from datetime import datetime
+import calendar
 
 @app.post('/fetchMonth')
 def fetch_month_year():
-    # try:
-        month = int(request.form.get("month"))  
+    try:
+        month = int(request.form.get("month"))
         year = int(request.form.get("year"))
 
         start_date = datetime(year, month, 1)
-        end_date = datetime(year, month + 1 if month < 12 else 1, 1)  
+        if month == 12:
+            end_date = datetime(year + 1, 1, 1)
+        else:
+            end_date = datetime(year, month + 1, 1)
 
         collection = d.db['user-exp-collection']
         cursor = collection.find({
             'Expense-date': {
-                '$gte': start_date,
-                '$lt': end_date
+                '$gte': start_date.strftime("%Y-%m-%d"),
+                '$lt': end_date.strftime("%Y-%m-%d")
             }
         })
 
@@ -27,5 +31,5 @@ def fetch_month_year():
 
         return render_template('fetchAll.html', expenses=expenses)
 
-    # except Exception as e:
-    #     return jsonify({'error': str(e)}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
